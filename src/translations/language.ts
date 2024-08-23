fe72a4c9695b35a1fda81891a487ebe0c8437028
luigi-raynel-dev <devluigiraynel@gmail.com>
2024-08-23 08:42:29 -0300

    fix: verify if navigator.language is included in system languages

--- src/translations/language.ts
+++ src/translations/language.ts
@@ -1,6 +1,8 @@
 const isoLanguages = ['en', 'pt-BR']
 
 export const getSystemLanguage = () => {
+  if (isoLanguages.includes(navigator.language)) return navigator.language
+
   const isoLang = navigator.language.slice(0, 2)
 
   const isoLanguage = isoLanguages.find(iso => iso === isoLang)
